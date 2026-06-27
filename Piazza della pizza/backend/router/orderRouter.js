27eef377bad40a55b5ad3856e8fe3076a8c14bc4
Soumeya <81039946+soumeyabmj@users.noreply.github.com>
2026-06-27 02:16:50 +0100

    Refactor orderRouter to CommonJS and add validation
    Refactor orderRouter to use CommonJS syntax and add validation middleware for order creation.

--- Piazza della pizza/backend/router/orderRouter.js
+++ Piazza della pizza/backend/router/orderRouter.js
@@ -1,12 +1,19 @@
-import { Router } from "express";
-import { createOrder, deleteOrder, getAllOrders, getOrder, updateOrder } from "../controllers/orderController.js";
+const { Router } = require('express');
+const {
+  createOrder,
+  getAllOrders,
+  getOrder,
+  updateOrder,
+  deleteOrder,
+} = require('../controllers/orderController');
+const { validateOrder } = require('../middleware/validate');
 
 const orderRouter = Router();
 
-orderRouter.get("/", getAllOrders);
-orderRouter.get("/:id", getOrder);
-orderRouter.post("/create", createOrder);
-orderRouter.delete("/:id", deleteOrder);
-orderRouter.put("/:id", updateOrder);
+orderRouter.get('/',         getAllOrders);
+orderRouter.get('/:id',      getOrder);
+orderRouter.post('/create',  validateOrder, createOrder);
+orderRouter.put('/:id',      updateOrder);
+orderRouter.delete('/:id',   deleteOrder);
 
-export default orderRouter;
+module.exports = orderRouter;
